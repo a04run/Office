7e3e56e8e6fdd64d9d49b4468efc3763f74c665b
a04run <a04run@gmail.com>
2017-05-22 07:28:38 +0530

    POP up initial
    POP up files with an example

---
+++ POP up/pop.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$(".popup").hide();
+	$(".trigger").click(function(){
+		$(".popup").show();
+	});
+	$(".close").click(function(){
+		$(".popup").hide();
+	})
+	
+})
